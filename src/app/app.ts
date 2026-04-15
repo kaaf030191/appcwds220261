306132e7e0335eb2e2fdedfb1a03638043433781
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2026-04-14 19:46:06 -0500

    Feat: 14-04-2026 - Primer consumido.
    Endpoint consumido, vía Get.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GeneralService } from './api/general-service';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	templateUrl: './app.html',
+	styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('appcwds220261');
+	constructor(
+		private generalService: GeneralService
+	) {}
+
+	ngOnInit(): void {
+		this.generalService.getIndex().subscribe({
+			next: (resposne: any) => {
+				console.log(resposne);
+			},
+			error: (error: any) => {
+				console.log(error);
+			}
+		});
+	}
 }
