1b994e8a3bb4e227c2dee723c4ad0db7f7678103
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2026-05-12 19:57:38 -0500

    Feat: 12-05-2026 - Implementación de Tailwind.
    Instalación y configuración inicial de Tailwind.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SuggestionInsert } from './page/suggestion/suggestion-insert/suggestion-insert';
+import { ComplaintInsert } from './page/complaint/complaint-insert/complaint-insert';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: 'suggestion/insert', component: SuggestionInsert },
+	{ path: 'complaint/insert', component: ComplaintInsert }
+];
